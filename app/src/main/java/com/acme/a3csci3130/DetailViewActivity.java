4b3c9bf1e6f52cd94ba2fe71b1523b8448f9e6d5
Brandon <brandongranbrem@gmail.com>
2017-07-06 09:16:30 -0300

    Updated firebase rules for data formatting, implemented delete and edit features for business data entries

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -9,12 +9,14 @@ public class DetailViewActivity extends Activity {
 
     private EditText businessNumberField, nameField, primaryBusinessField, addressField, provinceField;
     Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
+        appState = ((MyApplicationData) getApplicationContext());
 
         businessNumberField = (EditText) findViewById(R.id.businessNumber);
         nameField = (EditText) findViewById(R.id.name);
@@ -32,11 +34,24 @@ public class DetailViewActivity extends Activity {
     }
 
     public void updateContact(View v){
-        //TODO: Update contact funcionality
+        //Retrieve the key of the business being updated
+        String personID = receivedPersonInfo.uid;
+        //Retrieve the updated business information
+        String businessNumber = businessNumberField.getText().toString();
+        String name = nameField.getText().toString();
+        String business = primaryBusinessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+        //Create a new business contact
+        Contact person = new Contact(personID, businessNumber, name, business, address, province);
+        //Push the updated business contact to the database and list
+        appState.firebaseReference.child(personID).setValue(person);
+        finish();
     }
 
-    public void eraseContact(View v)
-    {
-        //TODO: Erase contact functionality
+    public void eraseContact(View v){
+        //Remove the contact from the database and list
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        finish();
     }
 }
